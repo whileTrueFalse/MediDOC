24870dfe5d3052d383174ee2e48d1a56d35ac541
whileTrueFalse <117916649+whileTrueFalse@users.noreply.github.com>
2024-05-17 09:05:02 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 
 # load databasedataset===================================
-sym_des = pd.read_csv("datasets\symtoms_df.csv")
-precautions = pd.read_csv("datasets\precautions_df.csv")
-workout = pd.read_csv("datasets\workout_df.csv")
-description = pd.read_csv("datasets\description.csv")
-medications = pd.read_csv('datasets\medications.csv')
-diets = pd.read_csv("datasets\diets.csv")
+sym_des = pd.read_csv("datasets/symtoms_df.csv")
+precautions = pd.read_csv("datasets/precautions_df.csv")
+workout = pd.read_csv("datasets/workout_df.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv('datasets/medications.csv')
+diets = pd.read_csv("datasets/diets.csv")
 
 
 # load model===========================================
-svc = pickle.load(open('models\svc (1).pkl','rb'))
+svc = pickle.load(open('models/svc (1).pkl','rb'))
 
 
 #============================================================
